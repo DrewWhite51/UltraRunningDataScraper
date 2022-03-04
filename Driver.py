bc48aa983fcdb097dda3440bda7e098f6bf742bc
Drew White <drew.m.white51@gmail.com>
2022-03-04 08:54:09 -0600

    Working on Python MongoDb connection.

--- Driver.py
+++ Driver.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import pandas
 from Race import Race
+from pymongo import MongoClient
+import pprint
 
 results_dataframe = pandas.read_csv('race_results_urls.csv')
 
 firstRace = Race('https://calendar.ultrarunning.com/event/hundred-in-the-hood/race/1096/results')
-
-
-
 
 def create_dataframe(race_url):
     try:
@@ -24,7 +23,7 @@ def create_dataframe(race_url):
         data['genders'] = (race.get_genders())
         data['ages'] = (race.get_ages())
         data['names'] = (race.get_names())
-        # data['raceName'] = race.get_race_name()
+        data['raceName'] = race.get_race_name()
         
         print(len(data['genders']))
         print(len(data['ages']))
@@ -39,7 +38,6 @@ def create_dataframe(race_url):
         print(race_url)
         
     
-print(create_dataframe(results_dataframe['0'][1]))
 
-# print(firstRace.get_genders())
+
 
